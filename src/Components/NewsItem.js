538dcd5a638c268b44ff7dd8bb537b806fab44ef
Abdullah-Zeb <abdullahzeb589@gmail.com>
2023-02-04 01:25:34 -0800

    Class Base Components in React js

---
+++ src/Components/NewsItem.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export class NewsItem extends Component {
+  render() {
+    return <div>this is a news item Component</div>;
+  }
+}
+
+export default NewsItem;
